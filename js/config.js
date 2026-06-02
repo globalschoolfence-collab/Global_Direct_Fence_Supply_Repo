2ebf96ad3ffc2214ae123658804bdcf8ff4f487c
globalschoolfence-collab <globalschoolfence-collab@users.noreply.github.com>
2026-06-02 02:46:54 +0000

    deploy: caf44ac2accd8a4876da35b105a9fa90e370e31a

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window.APP_CONFIG = {
+  scriptUrl:   'https://script.google.com/macros/s/AKfycbzhxdACRYrAHNeS76el1TzsdMh1v1maHJfErOQXMNvZivPfEf9VOQ-ehIrDiO-iddyFIw/exec',
+  secretToken: 'GSF-Reviews&Announcement-$$##^^2026',
+  ytApiKey:    '',
+  ytChannelId: 'UCLDSpvo-my7QOc-LAamay1Q',
+  sendQuoteUrl: 'https://us-central1-globaldirectfenceemail.cloudfunctions.net/sendQuote'
+};
