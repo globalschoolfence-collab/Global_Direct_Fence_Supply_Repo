03d5b02a32982c2d1afe307e717efb2485f095fc
globalschoolfence-collab <globalschoolfence-collab@users.noreply.github.com>
2026-06-01 22:40:52 -0400

    Move send-quote cloud function URL to config

--- js/config.example.js
+++ js/config.example.js
@@ -4,5 +4,6 @@ window.APP_CONFIG = {
   scriptUrl:   'YOUR_GOOGLE_APPS_SCRIPT_URL_HERE',
   secretToken: 'YOUR_SECRET_TOKEN_HERE',
   ytApiKey:    'YOUR_YOUTUBE_API_KEY_HERE',
-  ytChannelId: 'YOUR_YOUTUBE_CHANNEL_ID_HERE'
+  ytChannelId: 'YOUR_YOUTUBE_CHANNEL_ID_HERE',
+  sendQuoteUrl: 'YOUR_CLOUD_FUNCTION_URL_HERE'
 };
